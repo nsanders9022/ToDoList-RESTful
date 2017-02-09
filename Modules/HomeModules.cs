2a690f7fca2e6986e23dc878c03bbf7c1cfa07a7
Alexandra Holcombe and Nicole Sanders <alexandra+nicole@>
2017-02-09 09:42:34 -0800

    add comments to HomeModules

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -11,18 +11,23 @@ namespace ToDoList
       Get["/"] = _ => {
         return View["index.cshtml"];
       };
+      //Displays the list of the categories (returns the list called instances)
       Get["/all_categories"] = _ => {
         var allCategories = Category.GetAll();
         return View["all_categories.cshtml", allCategories];
       };
+      //When we select "Add a new category" on the index page, open the category form page
       Get["/all_categories/new"] = _ => {
         return View["category_form.cshtml"];
       };
+      //Takes info from the form and makes it into a new object. Then we get all of the category objects and show them all on the page
       Post["/all_categories"] = _ => {
         var newCategory = new Category(Request.Form["category-name"]);
         var allCategories = Category.GetAll();
         return View["all_categories.cshtml", allCategories];
       };
+      //List of all of the tasks in a single category
+      //Creates a dictionary with the key as either a 'categry' or a 'task', and the value as the applicable value and task
       Get["/all_categories/{id}"] = parameters => {
         Dictionary<string, object> model = new Dictionary<string, object>();
         var selectedCategory = Category.Find(parameters.id);
@@ -31,6 +36,8 @@ namespace ToDoList
         model.Add("tasks", categoryTasks);
         return View["category.cshtml", model];
       };
+      //Adding a task to a selected category
+      //Displays the form to add a task to that category
       Get["/all_categories/{id}/tasks/new"] = parameters => {
         Dictionary<string, object> model = new Dictionary<string, object>();
         Category selectedCategory = Category.Find(parameters.id);
@@ -39,10 +46,11 @@ namespace ToDoList
         model.Add("tasks", allTasks);
         return View["category_tasks_form.cshtml", model];
       };
+      //list of all the tasks in a single category
       Post["/tasks"] = _ => {
         Dictionary<string, object> model = new Dictionary<string, object>();
-        Category selectedCategory = Category.Find(Request.Form["category-id"]);
-        List<Task> categoryTasks = selectedCategory.GetTasks();
+        Category selectedCategory = Category.Find(Request.Form["category-id"]);//strongly typed
+        List<Task> categoryTasks = selectedCategory.GetTasks();// strongly typed. Both are just a different way to type var SelecteCategory etc from previous Get method.
         string taskDescription = Request.Form["task-description"];
         Task newTask = new Task(taskDescription);
         categoryTasks.Add(newTask);
@@ -50,6 +58,9 @@ namespace ToDoList
         model.Add("category", selectedCategory);
         return View["category.cshtml", model];
       };
+
+
+      //OLD CODE BEFORE WE ADDED THE CATEGORIES
       // Get["/tasks"] = _ => {
       //   List<Task> allTasks = Task.GetAll();
       //   return View["tasks.cshtml", allTasks];
